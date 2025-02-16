d651dab4bd703c253cfb290fe981fc063623f9ed
Hassan <hassanmousavi400@gmail.com>
2025-02-16 20:10:55 +0330

    responsive-p2

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const btnRight = document.querySelector(".slider__btn--right");
 const btnLeft = document.querySelector(".slider__btn--left");
 
 let curSlide = 0;
+let curSlide_1 = 0;
 const maxSlides = slides.length;
 const goToSlide = function (slide) {
   slides.forEach(
@@ -53,25 +54,28 @@ const goToSlide_1 = function (slide) {
   );
 };
 const nextSlide_1 = function () {
-  if (curSlide === maxSlides_1 - 5) {
-    curSlide = 0;
+  if (curSlide_1 === maxSlides_1 - 5) {
+    curSlide_1 = 0;
   } else {
-    curSlide++;
+    curSlide_1++;
   }
-  goToSlide_1(curSlide);
+  goToSlide_1(curSlide_1);
 };
 const prevSlide_1 = function () {
-  if (curSlide === 0) {
-    curSlide = maxSlides_1 - 5;
+  if (curSlide_1 === 0) {
+    curSlide_1 = maxSlides_1 - 5;
   } else {
-    curSlide--;
+    curSlide_1--;
   }
-  goToSlide_1(curSlide);
+  goToSlide_1(curSlide_1);
 };
 // clear codeing
 const init_1 = function () {
   goToSlide_1(0);
 };
+// setInterval(() => nextSlide(curSlide), 4000);
+// setInterval(() => nextSlide_1(curSlide_1), 4000);
+
 // EventHandlers new
 btnRight_1.addEventListener("click", nextSlide_1);
 btnLeft_1.addEventListener("click", prevSlide_1);
