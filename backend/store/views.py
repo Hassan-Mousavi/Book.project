dda5db788b60325ee083801acdda62933f839818
moslem <moslemamiri82@gmail.com>
2025-02-22 15:58:12 +0330

    add sign up and refactor views.py

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
 
 from .models import Book
 
 
-def book_list(request):
-    books = Book.objects.select_related('category').all()
-    return render(request, 'home.html', {'books': books})
+class BookListView(generic.ListView):
+    template_name = 'home.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        return Book.objects.select_related('category').all()
