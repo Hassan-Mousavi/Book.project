b8f3a135ae01d77220f5d42fb47112e71f9a7eb2
moslem <moslemamiri82@gmail.com>
2025-05-11 15:59:00 +0330

    add book_list

--- backend/store/views.py
+++ backend/store/views.py
@@ -3,8 +3,16 @@ from django.views import generic
 from .models import Book
 
 
-class BookListView(generic.ListView):
+class HomeView(generic.ListView):
     template_name = 'home.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        return Book.objects.select_related('category').all()
+
+
+class BookListView(generic.ListView):
+    template_name = 'store/book_list.html'
     context_object_name = 'books'
 
     def get_queryset(self):
