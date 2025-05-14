4087a8543d3a5b39636bc973f5637415725e3a30
moslem <moslemamiri82@gmail.com>
2025-05-14 22:03:54 +0330

    add about us

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('', include('accounts.urls')),
+    path('', include('pages.urls')),
     path("__debug__", include("debug_toolbar.urls")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
